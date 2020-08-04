6800ac5522ff97e4e34c7f3c05769aae8e68ebce
alirueter <rueter.alison@gmail.com>
2020-08-04 07:08:54 -0500

    POST tested and working with Insomnia

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const {animals} = require('./data/animals.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+//parse incoming JSON data
+app.use(express.json());
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     //saving animalsArray as filteredResults here:
@@ -64,6 +69,12 @@ app.get('/api/animals/:id', (req, res) => {
     else {
         res.send(404);
     }
+});
+
+app.post('/api/animals', (req, res) => {
+    //req.body is where incoming content will be
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
